da3f02a82827f5d4dea00f04850b175651d99b7b
Manav <51614345+plutoniumm@users.noreply.github.com>
2022-09-09 20:42:11 +0530

    Upd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,10 @@
 
 <section>
 	<h1>What's Happening?</h1>
+	<h2>
+		This page is for testing of web features and not for usage. The Links
+		don't work.
+	</h2>
 	<Posts posts={data.posts} />
 	<hr class="w-50 o-25" style="margin: 3rem auto;" />
 	<h1>Feed</h1>
