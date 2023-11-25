4fb8f3f452eeb8eb36a113d0c718ef160584582a
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-11-25 13:49:50 +0000

    Deploying to gh-pages from @ plutoniumm/ifactorial@a2ebf870975e07a7ba43e6a639ed2d915ce52758 🚀

---
+++ _app/immutable/chunks/3-2d7d2362.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-618c3865.js";import{default as t}from"../components/pages/_slug_/_page.svelte-0f28c5e3.js";export{t as component,r as universal};
---
+++ _app/immutable/modules/pages/_slug_/_page.js-b3c5904c.js
@@ -0,0 +1,1 @@
+import{l}from"../../../chunks/_page-618c3865.js";export{l as load};
